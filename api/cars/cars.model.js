5208be53b81da45fa91e3cd0900e0731a5998f01
tucker <tuckerwray84@gmail.com>
2020-02-03 01:22:13 -0500

    setup routes folder &config knexfile

---
+++ api/cars/cars.model.js
@@ -0,0 +1,20 @@
+function find() {
+    return db('cars').select('id', 'make', 'model');
+  }
+  
+  function findBy(filter) {
+    return db('cars').where(filter);
+  }
+  
+  async function add(car) {
+    const [id] = await db('cars').insert(car);
+  
+    return findById(id);
+  }
+  
+  function findById(id) {
+    return db('cars')
+      .where({ id })
+      .first();
+  }
+  
